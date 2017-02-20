cc5b177370a908079b60693a973162212279d3c4
Wolfkhan66 <cailehwald@googlemail.com>
2017-02-20 22:02:55 +0000

    Implement the Guide and Create pages and controllers for NPC's, Items and Locations

---
+++ InExile/Controllers/CreateController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InExile.Controllers
+{
+    public class CreateController : Controller
+    {
+        // GET: Create
+        public ActionResult Location()
+        {
+            return View();
+        }
+
+        public ActionResult NPC()
+        {
+            return View();
+        }
+
+        public ActionResult Item()
+        {
+            return View();
+        }
+    }
+}
---
+++ InExile/Controllers/GuideController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InExile.Controllers
+{
+    public class GuideController : Controller
+    {
+        // GET: Guide
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult NPCs()
+        {
+            return View();
+        }
+        public ActionResult Items()
+        {
+            return View();
+        }
+        public ActionResult Locations()
+        {
+            return View();
+        }
+    }
+}
