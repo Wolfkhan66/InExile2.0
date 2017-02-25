763334aace060e434f1d2929bf5c218c0901b614
Wolfkhan66 <cailehwald@googlemail.com>
2017-02-25 18:50:22 +0000

    Fix bad view routing in UpdateController, Create basic success page for Updates, Remove unnecessary code from guide pages

--- InExile/Controllers/UpdateController.cs
+++ InExile/Controllers/UpdateController.cs
@@ -13,21 +13,21 @@ namespace InExile.Controllers
             {
                 GuideLocation location = new GuideLocation(name, description, additional);
                 CreateParameters(location);
-                return View("Guide");
+                return View("Success");
             }
 
             public ActionResult NPC(string name, string appearance, string location, string additional)
             {
                 GuideNPC npc = new GuideNPC(name, appearance, location, additional);
                 CreateParameters(npc);
-                return View("Guide");
+                return View("Success");
             }
 
             public ActionResult Item(string name, string description, string additional)
             {
                 GuideItem item = new GuideItem(name, description, additional);
                 CreateParameters(item);
-                return View("Guide");
+                return View("Success");
             }
 
             public void CreateParameters(GuideLocation location)
